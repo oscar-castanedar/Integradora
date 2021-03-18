264d2c07e3c3484c2031b1d9f30acfbd19073f9c
SaulReyes-ux <79472319+SaulReyes-ux@users.noreply.github.com>
2021-03-18 13:43:31 -0600

    Update Periodo.php

--- app/Models/Periodo.php
+++ app/Models/Periodo.php
@@ -7,6 +7,10 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
  * Funcionalidad: Administrar nuevos periodos.
  * Autor: Rodríguez Flores Raúl Alberto
  * Fecha: 17/03/2021
+ * Descripción: Mostras el perido activo en la pantalla docente
+ * Funcionalidad: Mostrar los periodos activos y inactivos(solo se podra ver los curosos pero no se podra ingrear a ellos)
+ * Autor: Marin Reyes Saul Guadalupe
+ * Fecha: 18/03/2021
  */
 
 class Periodo extends Eloquent{
