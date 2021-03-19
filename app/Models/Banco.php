960e261528ddb77a53fb08f0e2e78398a4a82b00
Juan-salas2 <80724090+Juan-salas2@users.noreply.github.com>
2021-03-19 13:21:49 -0600

    Update Banco.php

--- app/Models/Banco.php
+++ app/Models/Banco.php
@@ -6,6 +6,10 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 * Descripción: El Modelo de crear pregutas para el banco de reactivos.
 * Autor: José Alejandro Cruz Medina 
 * Fecha: 17/03/2021
+* Descripción: Vizualizar el curso una vez que esta ya haya sido creado y realizar modificaciomes.
+* Funcionalidad: Ver y / o editar el curso, una vez que este ya haya sido creado.
+* Autor: Cabello Salas Juan Carlos.
+* Fecha: 19/03/2020
 */
 
 class Pregunta extends Eloquent{
