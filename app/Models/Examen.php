363ad84fce58fb289604de71775927d0dcfb8855
Juan-salas2 <80724090+Juan-salas2@users.noreply.github.com>
2021-03-19 13:24:34 -0600

    Update Examen.php

--- app/Models/Examen.php
+++ app/Models/Examen.php
@@ -6,6 +6,10 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 * Descripción: El Modelo de crear Examen
 * Autor: José Alejandro Cruz Medina 
 * Fecha: 17/03/2021
+* Descripción: Vizualizar el curso una que este ya haya sido creado y realizar modificaciones.
+* Funcionalidad: Ver y / o editar el curso, una vez que este ya haya sido creado.
+* Autor: Cabello Salas Juan Carlos.
+* Fecha: 19/03/2020.
 */
 
 class Banco extends Eloquent{
